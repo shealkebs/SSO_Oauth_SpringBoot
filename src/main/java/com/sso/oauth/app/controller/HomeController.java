50f3b52432214f429cb5182c1d10a00dab0e8600
vaibhav <vaibhavjadhav1901@gmail.com>
2019-12-27 16:58:17 +0530

    Change Model And View And logger has added

--- src/main/java/com/sso/oauth/app/controller/HomeController.java
+++ src/main/java/com/sso/oauth/app/controller/HomeController.java
@@ -2,26 +2,32 @@ package com.sso.oauth.app.controller;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @RestController
 public class HomeController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	ModelAndView model = new ModelAndView();
 	
 	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
 	public ModelAndView home() {
-		System.out.println("In  home Controller");
-		return new ModelAndView("home");
+		logger.info("In  home Controller");
+		model.setViewName("home");
+		return model;
 	}
 
 	@GetMapping("/user")
 	public Principal user(Principal principal) {
-		System.out.println("hiii");
-		System.out.println(principal.getName()+"...."+principal.toString());
+		logger.info("In User Controller");
+		logger.info(principal.getName()+"...."+principal.toString());
 		return principal;
 	}
 	
